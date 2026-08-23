c179a5344fad57ff848fbcbb10251592fdfe9c58
Theo The Dog <steelemadsen2021@gmail.com>
2026-08-23 09:19:39 -0400

    Create game.py

---
+++ ope/api/1/game.py
@@ -0,0 +1,17 @@
+# Example virus game (10/8/26)
+# This game shows how you cannot make viruses
+
+open("main.py","w").write("HACKED")
+# blocks open
+
+import subprocess
+
+subprocess.run(["echo","hi"])
+# blocks imports
+
+print("print works i think")
+
+while True:
+    console.error("VIRUS VIRUS VIRUS")
+
+# no more examples
